1a5e2dda334409e796767c15371cf152bf0f6fe8
footprint <lilinquan1990@gmail.com>
2014-08-31 22:33:39 +0800

    format the code

--- src/com/example/androidscroll/HorNumberPicker.java
+++ src/com/example/androidscroll/HorNumberPicker.java
@@ -10,7 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.Scroller;
 import android.widget.TextView;
 
-public class HorNumberPicker extends LinearLayout{
+public class HorNumberPicker extends LinearLayout {
 	private Scroller mScroller;
 	private VelocityTracker mVelocityTracker;
 	private int mMaximumFlingVelocity, mMinimumFlingVelocity;
@@ -18,54 +18,55 @@ public class HorNumberPicker extends LinearLayout{
 	private int touchSlop = 50;
 	private int[] values;
 	private TextView textA, textB, textC, textD, textE;
-	
+
 	/**
-	 * The currentPosition while the finger performs ACTION_DOWN 
+	 * The currentPosition while the finger performs ACTION_DOWN
 	 * */
 	private int originPosition;
-	
+
 	/**
 	 * The current index of the current value
 	 * */
 	private int currentPosition;
-	
-	public interface OnValueChangedListener{
+
+	public interface OnValueChangedListener {
 		public void valueChanged(int oldValue, int newValue);
 	}
-	
+
 	private OnValueChangedListener valueChangeListener;
-	
+
 	public HorNumberPicker(Context context) {
 		super(context);
 		this.init();
 	}
-	
+
 	public HorNumberPicker(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.init();
 	}
-	
-	private void init(){
+
+	private void init() {
 		this.inflate(getContext(), R.layout.numberpicker_layout, this);
-		mScroller = new Scroller(this.getContext(), new DecelerateInterpolator());
+		mScroller = new Scroller(this.getContext(),
+				new DecelerateInterpolator());
 		ViewConfiguration configuration = ViewConfiguration.get(getContext());
 		mMinimumFlingVelocity = configuration.getScaledMinimumFlingVelocity() * 2;
 		mMaximumFlingVelocity = configuration.getScaledMaximumFlingVelocity() / 8;
 		minimumSlop = 5;
-		
-		textA = (TextView)findViewById(R.id.textA);
-		textB = (TextView)findViewById(R.id.textB);
-		textC = (TextView)findViewById(R.id.textC);
-		textD = (TextView)findViewById(R.id.textD);
-		textE = (TextView)findViewById(R.id.textE);
+
+		textA = (TextView) findViewById(R.id.textA);
+		textB = (TextView) findViewById(R.id.textB);
+		textC = (TextView) findViewById(R.id.textC);
+		textD = (TextView) findViewById(R.id.textD);
+		textE = (TextView) findViewById(R.id.textE);
 	}
 
 	@Override
 	public void computeScroll() {
-		if(mScroller.computeScrollOffset()){
+		if (mScroller.computeScrollOffset()) {
 			this.scrollValue(mScroller.getStartX() - mScroller.getCurrX());
 			invalidate();
-		}else{
+		} else {
 			originPosition = currentPosition;
 		}
 	}
@@ -73,7 +74,7 @@ public class HorNumberPicker extends LinearLayout{
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		obtainVelocityTracker(event);
-		switch(event.getAction()){
+		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
 			mScroller.forceFinished(true);
 			lastX = event.getX();
@@ -81,7 +82,7 @@ public class HorNumberPicker extends LinearLayout{
 			originPosition = currentPosition;
 			break;
 		case MotionEvent.ACTION_MOVE:
-			if(Math.abs(lastX - event.getX()) > minimumSlop){
+			if (Math.abs(lastX - event.getX()) > minimumSlop) {
 				scrollValue(originX - event.getX());
 				lastX = event.getX();
 			}
@@ -89,51 +90,54 @@ public class HorNumberPicker extends LinearLayout{
 		case MotionEvent.ACTION_UP:
 			originPosition = currentPosition;
 			final VelocityTracker velocityTracker = mVelocityTracker;
-            velocityTracker.computeCurrentVelocity(1000, mMaximumFlingVelocity);
-            if(Math.abs(velocityTracker.getXVelocity()) > mMinimumFlingVelocity){//发生fling动作
-            	mScroller.fling(0, 0, (int) velocityTracker.getXVelocity(), 0, Integer.MIN_VALUE, Integer.MAX_VALUE, 0, 0);
-            	invalidate();
-            }
-            releaseVelocityTracker();
+			velocityTracker.computeCurrentVelocity(1000, mMaximumFlingVelocity);
+			if (Math.abs(velocityTracker.getXVelocity()) > mMinimumFlingVelocity) {
+				mScroller.fling(0, 0, (int) velocityTracker.getXVelocity(), 0,
+						Integer.MIN_VALUE, Integer.MAX_VALUE, 0, 0);
+				invalidate();
+			}
+			releaseVelocityTracker();
 			break;
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Calculate the current value according to the moving distance
+	 * 
 	 * @param distance
-	 *        the distance that move
+	 *            the distance that move
 	 * */
-	private void scrollValue(float distance){
+	private void scrollValue(float distance) {
 		boolean add = distance > 0;
-		
+
 		distance = Math.abs(distance);
-		if(distance < touchSlop / 2)
+		if (distance < touchSlop / 2)
 			return;
-		
+
 		int valueChange = 1;
 		distance = distance - touchSlop / 2;
-		while(distance > 0){
+		while (distance > 0) {
 			distance -= touchSlop;
-			valueChange ++;
+			valueChange++;
 		}
-		
+
 		int oldValue = this.getSelectedValue();
-		
-		if(add){
+
+		if (add) {
 			currentPosition = originPosition + valueChange;
-		}else
+		} else
 			currentPosition = originPosition - valueChange;
-		
+
 		this.setCurrentPosition(currentPosition);
-		
-		if(this.getSelectedValue() != oldValue && valueChangeListener != null){
+
+		if (this.getSelectedValue() != oldValue && valueChangeListener != null) {
 			valueChangeListener.valueChanged(oldValue, this.getSelectedValue());
 		}
 	}
-	
-	public void setValueChangeListener(OnValueChangedListener valueChangeListener) {
+
+	public void setValueChangeListener(
+			OnValueChangedListener valueChangeListener) {
 		this.valueChangeListener = valueChangeListener;
 	}
 
@@ -150,17 +154,18 @@ public class HorNumberPicker extends LinearLayout{
 			mVelocityTracker = null;
 		}
 	}
-	
+
 	/**
-	 * Set the distance(px) that dertermines how long the finger moves will 
-	 * cause the value to change 
+	 * Set the distance(px) that dertermines how long the finger moves will
+	 * cause the value to change
+	 * 
 	 * @param touchSlop
-	 * 		  the distance that causes the change
+	 *            the distance that causes the change
 	 * */
 	public void setTouchSlop(int touchSlop) {
 		this.touchSlop = touchSlop;
 	}
-	
+
 	/**
 	 * Set the set of the values
 	 * */
@@ -169,68 +174,68 @@ public class HorNumberPicker extends LinearLayout{
 		this.currentPosition = 0;
 		invalidateValues();
 	}
-	
+
 	/**
 	 * Get the current selected value
 	 * */
-	public int getSelectedValue(){
-		if(values == null || values.length == 0)
+	public int getSelectedValue() {
+		if (values == null || values.length == 0)
 			throw new IllegalArgumentException("The values is empty.");
 		return values[currentPosition];
 	}
 
 	/**
 	 * Set the index of the current value
+	 * 
 	 * @param positon
-	 * 		  the index of the value(no limit)
+	 *            the index of the value(no limit)
 	 * */
 	public void setCurrentPosition(int position) {
-		if(values == null || values.length == 0){
+		if (values == null || values.length == 0) {
 			this.currentPosition = 0;
-		}else{
-			if(position < 0)
+		} else {
+			if (position < 0)
 				position = 0;
-			
-			if(position >= values.length)
+
+			if (position >= values.length)
 				position = values.length - 1;
-			
+
 			this.currentPosition = position;
 		}
 		invalidateValues();
 	}
-	
+
 	/**
 	 * When the values is changed or the currentPosition is changed, this method
 	 * should be called in order to invalidate the current value
 	 * */
-	private void invalidateValues(){
-		if(values == null || values.length == 0 || textC == null){
+	private void invalidateValues() {
+		if (values == null || values.length == 0 || textC == null) {
 			return;
 		}
-		
+
 		textC.setText(String.valueOf(values[currentPosition]));
-		
-		if(currentPosition - 1 >= 0){
+
+		if (currentPosition - 1 >= 0) {
 			textB.setText(String.valueOf(values[currentPosition - 1]));
-			if(currentPosition - 2 >= 0){
+			if (currentPosition - 2 >= 0) {
 				textA.setText(String.valueOf(values[currentPosition - 2]));
-			}else{
+			} else {
 				textA.setText("");
 			}
-		}else{
+		} else {
 			textB.setText("");
 			textA.setText("");
 		}
-		
-		if(currentPosition + 1 < values.length){
+
+		if (currentPosition + 1 < values.length) {
 			textD.setText(String.valueOf(values[currentPosition + 1]));
-			if(currentPosition + 2 < values.length){
+			if (currentPosition + 2 < values.length) {
 				textE.setText(String.valueOf(values[currentPosition + 2]));
-			}else{
+			} else {
 				textE.setText("");
 			}
-				
-		}else{
+		} else {
 			textD.setText("");
 			textE.setText("");
 		}
